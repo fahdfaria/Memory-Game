0e66afd9f91b7487a3b45b92711c5cebe9d705f0
Fahd <fahdf@uw.edu>
2018-12-30 22:42:07 -0800

    modified urls in js and added styles + copyright

--- scripts/script.js
+++ scripts/script.js
@@ -71,7 +71,6 @@ const imagesArray = [
 
       var front = document.createElement("div");
       front.classList.add("front");
-      front.style.background = "url('./images/Question_mark_card.png') no-repeat"
 
       var back = document.createElement("div");
       back.classList.add("back")
